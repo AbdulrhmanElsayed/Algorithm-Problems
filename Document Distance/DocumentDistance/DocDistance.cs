1b26d0c7236671f6661a56537857ec1251d6ba30
AbdulrhmanElsayed <48297159+AbdulrhmanElsayed@users.noreply.github.com>
2019-10-30 23:50:09 +0200

    Algorithm solved problems

---
+++ Document Distance/DocumentDistance/DocDistance.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace DocumentDistance
+{
+    class DocDistance
+    {
+        // ***************
+        // DON'T CHANGE CLASS OR FUNCTION NAME
+        // YOU CAN ADD FUNCTIONS IF YOU NEED TO
+        // ***************
+        /// <summary>
+        /// Write an efficient algorithm to calculate the distance between two documents
+        /// </summary>
+        /// <param name="doc1FilePath">File path of 1st document</param>
+        /// <param name="doc2FilePath">File path of 2nd document</param>
+        /// <returns>The angle (in degree) between the 2 documents</returns>
+        public static string taketxt(string docPath)
+        {
+            string f = "";
+            f = File.ReadAllText(docPath);
+            return f;
+        }
+
+        public static double CalculateDistance(string doc1FilePath, string doc2FilePath)
+        {
+            Regex regex = new Regex("[^A-Za-z0-9]");
+            long sumation_Ds = 0, D1 = 0, D2 = 0, makam;
+            double Gazrmakam;
+            doc1FilePath = doc1FilePath.Replace('\'', '\\');
+            doc2FilePath = doc2FilePath.Replace('\'', '\\');
+            // TODO comment the following line THEN fill your code here
+            IDictionary<string, long> d1 = new Dictionary<string, long>();
+            IDictionary<string, long> d2 = new Dictionary<string, long>();
+
+            string f1 = taketxt(doc1FilePath);
+            string f2 = taketxt(doc2FilePath);
+
+            f1 = regex.Replace(f1, "#").ToLower();
+            f2 = regex.Replace(f2, "#").ToLower();
+            f1 = f1 + "#";
+            f2 = f2 + "#";
+            // Console.WriteLine(f1);
+            // Console.WriteLine(f2);
+            string x = "";
+            for (long i = 0; i < f1.Length; i++)
+            {
+
+                if (f1[i] != '#')
+                {
+                    x += f1[i];
+                }
+                else
+                {
+                    if (x == "#"|| x == "")
+                        continue;
+                    if (d1.ContainsKey(x))
+                    {
+                        d1[x] += 1;
+                    }
+                    else if(!d1.ContainsKey(x)&& f1[i].Equals('#'))
+                    {
+                        d1.Add(x, 1);
+                    }
+                    x = "";
+                }
+            }
+            x = "";
+            for (long j = 0; j < f2.Length; j++)
+            {
+
+                if (f2[j] != '#')
+                {
+                    x += f2[j];
+                }
+                else
+                {
+                    if (x == "#" || x == "")
+                        continue;
+                    if (d2.ContainsKey(x))
+                    {
+                        d2[x] += 1;
+                    }
+                    else if (!d2.ContainsKey(x) && f2[j].Equals('#'))
+                    {
+                        
+                        d2.Add(x, 1);
+                    }
+                    x = "";
+                }
+            }
+            foreach (string it in d1.Keys)
+            {
+                if (d2.ContainsKey(it))
+                {
+                    sumation_Ds += d1[it] * d2[it];
+                }
+
+            }
+            foreach (long s in d1.Values)
+            {
+                D1 += s*s;
+            }
+            foreach (long s in d2.Values)
+            {
+                D2 += s * s;
+            }
+            makam = D1 * D2;
+            Gazrmakam = Math.Sqrt(makam);
+            double dis = Math.Acos(sumation_Ds / Gazrmakam);
+            dis = (dis * 180) / Math.PI;
+
+            return dis;
+
+        }
+    }
+}
---
+++ [1] Currency Exchange/CurrencyExchange/CurrencyExchange/CurrencyExchange.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange
+{
+    // *****************************************
+    // DON"T CHANGE CLASS NAME OR FUNCTION NAME
+    // *****************************************
+    public static class CurrencyExchange
+    {
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// find the index of the USD Dollar $ with the smallest number of questions.
+        /// </summary>
+        /// <param name="N">Number of customers (N)</param>
+        /// <param name="M">Number of currencies (M)</param>
+        /// <param name="knows">N*M Matrix indicating whether customer i knows currency j or not</param>
+        /// <returns>index of US Dollar</returns>
+        public static int CheckUSD(int N, int M, bool[,] knows)
+        {
+
+            return searchTrue(knows,N,M,0,0);
+        }
+        public static int searchTrue(bool[,] arr,int N, int M,int row,int col)
+        {
+            if(row == N - 1 && arr[row,col] == true)
+            {
+                return col;
+            }
+            else if (arr[row, col])
+            {
+                row++;
+                return searchTrue(arr, N, M, row, col);
+            }
+            else if (!arr[row, col])
+            {
+                col++;
+                return searchTrue(arr, N, M, row, col);
+            }
+
+            return -1;
+        }
+        
+
+        }
+    }
